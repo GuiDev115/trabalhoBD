098a45daaf05ac85a6613780c7af4060c66f9675
zk1Ryuu <guilherme.fabricio.115@gmail.com>
2023-02-14 00:38:29 -0300

    Identacao | Show all Tuples | UI better

--- incluir.php
+++ incluir.php
@@ -1,15 +1,23 @@
 <?php
+
   include("./config.php");
+
   $con = mysqli_connect($host, $login, $senha, $bd);
+
   if(isset($_POST["idrestaurante"])){
     $sql = "SELECT idrestaurante FROM restaurante WHERE idrestaurente=".$_POST["idrestaurante"];
+
     $result = mysqli_query($con, $sql);
+
     if(mysqli_num_rows($result)!=0){
-      $sql = "UPDATE restaurante SET nome='".$_POST["nomerestaurante"]."',cnpj=".$_POST["cnpj_rest"].",numero=".$_POST["numero"]." WHERE codigo=".$_POST["idrestaurente"];
+      $sql = "UPDATE restaurante SET nome='".$_POST["nomerestaurante"]."',cnpj=".$_POST["cnpj_rest"].
+        ",numero=".$_POST["numero"]." WHERE codigo=".$_POST["idrestaurente"];
     }
-  }else{
-    $sql = "INSERT INTO restaurante VALUES (null,'".$_POST["nomerestaurante"]."',".$_POST["cnpj_rest"].",".$_POST["numero"].")";
   }
+  
+  else
+    $sql = "INSERT INTO restaurante VALUES (null,'".$_POST["nomerestaurante"]."',".$_POST["cnpj_rest"].",".$_POST["numero"].")";
+  
   mysqli_query($con, $sql);
   mysqli_close($con);
   header("location: ./index.php");
--- index.php
+++ index.php
@@ -2,59 +2,102 @@
 header('Content-type: text/html; charset=utf-8');
 ?>
 <html>
-<head><title>Agenda Telefônica.</title></head>
-<body>
-<center><h3>Agenda Telefônica</h3></center>
-<form name="form1" method="POST" action="form_incluir.php">
-<table border="0" align="center" width="60%">
 
-<?php
-include("./config.php");
-$con = mysqli_connect($host, $login, $senha, $bd);
+  <head>
+    <title>Agenda Telefônica.</title>
+  </head>
 
+  <body>
+    <center><h3>Agenda Telefônica</h3></center>
+    <form name="form1" method="POST" action="form_incluir.php">
+    <table border="0" align="center" width="60%">
 
-$sql = "SELECT * FROM restaurante";
-$tabela = mysqli_query($con, $sql);
-if(mysqli_num_rows($tabela)==0){
-?>  
+    <?php
+      include("./config.php");
+      $con = mysqli_connect($host, $login, $senha, $bd);
 
-  <tr><td align="center">Não há nenhum contato cadastrado.</td></tr>
-  <tr><td align="center"><input type="submit" value="incluir Contato"></td></tr>
+      $sql = "SELECT * FROM restaurante";
+      $tabela = mysqli_query($con, $sql);
+      if(mysqli_num_rows($tabela)==0){
+    ?>  
 
-<?php
-}else{
-?>
-	<tr bgcolor="grey"><td width="30%">Nome</td><td width="20%">CNPJ</td><td width="20%">Numero</td><td width="30%"></td></tr>
-<?php
-  while($dados = mysqli_fetch_row($tabela)){
-?>
+      <tr>
+        <td align="center">Não há nenhum contato cadastrado.</td>
+      </tr>
 
+      <tr>
+        <td align="center"><input type="submit" value="incluir Contato"></td>
+      </tr>
 
-  <tr><td><?php echo $dados[1]; ?></td>
-      <td><?php echo $dados[2]; ?></td>
-      <td><?php echo $dados[3]; ?></td>
-	  <td align="center">
-	    <input type="button" value="Excluir" onclick="location.href='exclu7ir.php?codigo=<?php echo $dados[0]; ?>'">
-	    <input type="button" value="Editar" onclick="location.href='form_incluir.php?codigo=<?php echo $dados[0]; ?>'">
-	  </td>
-  </tr>
+    <?php
+      }
+      
+      else{
+    ?>
 
-<?php
-  }
-?>
-
-<tr bgcolor="grey"><td colspan="3" height="5"></td></tr>
-
-<?php
-mysqli_close($con);
-?>
-<tr><td colspan="3" align="left"><input type="submit" value="Incluir Novo Contato"></td></tr>
-<?php
-}
+	    <tr bgcolor="grey">
+        <td width="16%">Nome</td>
+        <td width="16%">CNPJ</td>
+        <td width="16%">Horario Abre</td>
+        <td width="16%">Horario Fecha</td>
+        <td width="16%">Logradouro</td>
+        <td width="16%">Numero</td>
+      </tr>
 
 
-?>
-</table>
-</form>
-</body>
+    <?php
+      while($dados = mysqli_fetch_row($tabela)){
+    ?>
+
+        <tr>
+          <td><?php echo $dados[1]; ?></td>
+          <td><?php echo $dados[2]; ?></td>
+          <td><?php echo $dados[3]; ?></td>
+          <td><?php echo $dados[4]; ?></td>
+          <td><?php echo $dados[5]; ?></td>
+          <td><?php echo $dados[6]; ?></td>
+	        <td align="center">
+	        </td>
+        </tr>
+
+
+      <tr bgcolor="grey">
+        <td width="20%">Complemento</td>
+        <td width="20%">Cidade</td>
+        <td width="20%">Bairro</td>
+        <td width="20%">Estado</td>
+        <td width="20%">CEP</td>
+      </tr>
+
+        <tr>
+          <td><?php echo $dados[7]; ?></td>
+          <td><?php echo $dados[8]; ?></td>
+          <td><?php echo $dados[9]; ?></td>
+          <td><?php echo $dados[10]; ?></td>
+          <td><?php echo $dados[11]; ?></td>
+          <td><?php echo $dados[12]; ?></td>
+	        <td align="center">
+	        </td>
+        </tr>
+
+    <?php
+    }
+    ?>
+
+    <tr bgcolor="grey"><td colspan="3" height="5"></td></tr>
+
+    <?php
+      mysqli_close($con);
+    ?>
+
+    <tr><td colspan="3" align="left">
+      <input type="submit" value="Incluir Novo Contato">
+      <input type="button" value="Excluir" onclick="location.href='exclu7ir.php?codigo=<?php echo $dados[0]; ?>'">
+	    <input type="button" value="Editar" onclick="location.href='form_incluir.php?codigo=<?php echo $dados[0]; ?>'"></td></tr>
+    <?php
+      }
+    ?>
+      </table>
+    </form>
+  </body>
 </html>
